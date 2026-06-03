236430b89d0d19343c11fb6d679d774048f00b3c
edrich-rab <97443156+edrich-rab@users.noreply.github.com>
2026-06-03 11:12:38 -0700

    added fulladder.vhd file from lab 4

---
+++ FullAdder.vhd
@@ -0,0 +1,19 @@
+-- full adder from lab 4
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity FullAdder is
+    Port (
+        a    : in  STD_LOGIC;
+        b    : in  STD_LOGIC;
+        cin  : in  STD_LOGIC;
+        s    : out STD_LOGIC;
+        cout : out STD_LOGIC
+    );
+end FullAdder;
+
+architecture Behavioral of FullAdder is
+begin
+    s    <= a xor b xor cin;
+    cout <= (a and b) or (cin and (a xor b));
+end Behavioral;
